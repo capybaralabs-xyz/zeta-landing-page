c94a27cb0c5043d3fc5ea378b0835d1ce9fdec48
bella <bellawmm69@gmail.com>
2024-12-02 15:25:49 +0800

    feat: fix

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,25 +1,122 @@
-import React from "react";
-import { Box, Button, Flex,Text,Image } from "@chakra-ui/react";
-import Group1 from '../assets/Group1.png'
-import light from '../assets/light.png'
+import React from 'react';
+import { Box, Button, Flex, Text, Image } from '@chakra-ui/react';
+import Group1 from '../assets/Group1.png';
+import light from '../assets/light.png';
 export default function Card() {
-  return <Flex width={'100%'} p={{base: '0.8rem',sm: '1rem', md: '2rem',lg: '2.5rem',xl: '3rem'}} bg={'#fff'} boxShadow={'0px 6px 40px 0px rgba(0, 0, 0, 0.1)'} borderRadius={{base: '1rem', sm: '1.5rem',md: '2rem', lg: '3rem',xl: '4rem'}} gap={'1rem'} alignItems={'center'} justifyContent={'space-between'}>
-					<Flex direction={'column'} justifyContent={'space-between'} gap={'4rem'} width={'60%'}>
-						<Box gap={'1rem'}>
-							<Text fontWeight={'700'} fontSize={{base: '1.6rem',sm: '1.8rem', md: '2rem', lg: '2.2rem',xl: '2.4rem'}} whiteSpace={'nowrap'}>Trading Perpetual Contracts</Text>
-							<Text fontWeight={'500'} fontSize={{base: '1.2rem', xl: '1.6rem'}}>Trade without any slippage and counterparty risk with up to 100x leverage completely on-chain.</Text>
-						</Box>
-						<Box>
-							<Button variant={'normal'} mb={'0.5rem'} padding={{base: '1.5rem 1.5rem', sm: '1.8rem 2.5rem', md: '2rem 4.5rem', lg: '2.2rem 6rem', xl: '2.4rem 7rem'}} onClick={() => {}}>Trade Now</Button>
-							<Text fontWeight={'500'} fontSize={{base: '1.2rem', xl: '1.6rem'}} whiteSpace={'nowrap'}>Unlock the power of leverage with KTX.Finance now!</Text>
-						</Box>
-					</Flex>
-					<Flex position={'relative'} width={{base: '8rem',sm: '10rem',md: '10rem',lg: '10rem',xl: '12.8rem'}}>
-						<Image src={Group1} width={{base: '8rem',sm: '10rem',md: '10rem',lg: '10rem',xl: '12.8rem'}} minW={{base: '8rem',sm: '10rem',md: '10rem',lg: '10rem',xl: '12.8rem'}} height={{base: '8rem',sm: '10rem',md: '10rem',lg: '10rem',xl: '12.8rem'}} />
-						<Box position={'absolute'} width={'140%'} height={'140%'} borderRadius={'50%'} top={'-20%'} left={'-20%'} right={'-20%'} bottom={'-20%'}>
-							<Image src={light} width={'100%'} height={'100%'}/>
-						</Box>
-						{/* <Box width={'14rem'} height={'14rem'} position={'absolute'} filter={'blur(80.33px)'} top={'50%'} left={'50%'} m={'-7rem 0 0 -7rem'} bg={'rgba(255, 242, 195, 1)'}></Box> */}
-					</Flex>
-				</Flex>
+  return (
+    <Flex
+      width={'100%'}
+      p={{ base: '0.8rem', sm: '1rem', md: '2rem', lg: '2.5rem', xl: '3rem' }}
+      bg={'#fff'}
+      boxShadow={'0px 6px 40px 0px rgba(0, 0, 0, 0.1)'}
+      borderRadius={{
+        base: '1rem',
+        sm: '1.5rem',
+        md: '2rem',
+        lg: '3rem',
+        xl: '4rem',
+      }}
+      gap={'1rem'}
+      alignItems={'center'}
+      justifyContent={'space-between'}
+    >
+      <Flex
+        direction={'column'}
+        justifyContent={'space-between'}
+        gap={'4rem'}
+        width={'60%'}
+      >
+        <Box gap={'1rem'}>
+          <Text
+            fontWeight={'700'}
+            fontSize={{
+              base: '1.6rem',
+              sm: '1.8rem',
+              md: '2rem',
+              lg: '2.2rem',
+              xl: '2.4rem',
+            }}
+            whiteSpace={'nowrap'}
+          >
+            Trading Perpetual Contracts
+          </Text>
+          <Text fontWeight={'500'} fontSize={{ base: '1.2rem', xl: '1.6rem' }}>
+            Trade without any slippage and counterparty risk with up to 100x
+            leverage completely on-chain.
+          </Text>
+        </Box>
+        <Box>
+          <Button
+            variant={'normal'}
+            mb={'0.5rem'}
+            padding={{
+              base: '1.5rem 1.5rem',
+              sm: '1.8rem 2.5rem',
+              md: '2rem 4.5rem',
+              lg: '2.2rem 6rem',
+              xl: '2.4rem 7rem',
+            }}
+            onClick={() => {}}
+          >
+            Trade Now
+          </Button>
+          <Text
+            fontWeight={'500'}
+            fontSize={{ base: '1.2rem', xl: '1.6rem' }}
+            whiteSpace={'nowrap'}
+          >
+            Unlock the power of leverage with KTX.Finance now!
+          </Text>
+        </Box>
+      </Flex>
+      <Flex
+        position={'relative'}
+        width={{
+          base: '8rem',
+          sm: '10rem',
+          md: '10rem',
+          lg: '10rem',
+          xl: '12.8rem',
+        }}
+      >
+        <Image
+          src={Group1}
+          width={{
+            base: '8rem',
+            sm: '10rem',
+            md: '10rem',
+            lg: '10rem',
+            xl: '12.8rem',
+          }}
+          minW={{
+            base: '8rem',
+            sm: '10rem',
+            md: '10rem',
+            lg: '10rem',
+            xl: '12.8rem',
+          }}
+          height={{
+            base: '8rem',
+            sm: '10rem',
+            md: '10rem',
+            lg: '10rem',
+            xl: '12.8rem',
+          }}
+        />
+        <Box
+          position={'absolute'}
+          width={'140%'}
+          height={'140%'}
+          borderRadius={'50%'}
+          top={'-20%'}
+          left={'-20%'}
+          right={'-20%'}
+          bottom={'-20%'}
+        >
+          <Image src={light} width={'100%'} height={'100%'} />
+        </Box>
+        {/* <Box width={'14rem'} height={'14rem'} position={'absolute'} filter={'blur(80.33px)'} top={'50%'} left={'50%'} m={'-7rem 0 0 -7rem'} bg={'rgba(255, 242, 195, 1)'}></Box> */}
+      </Flex>
+    </Flex>
+  );
 }
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,34 +1,124 @@
-import React from "react";
-import { Box, Flex,Image,Text,Link } from "@chakra-ui/react";
-import Logo from '../assets/Logo.png'
-import { FooterLinks } from "../utils";
-import telegram from '../assets/telegram.png'
-import medium from '../assets/medium..png'
-import discord from '../assets/discord.png'
-import X_twitter from '../assets/X_twitter.png'
+import React from 'react';
+import { Box, Flex, Image, Text, Link } from '@chakra-ui/react';
+import Logo from '../assets/Logo.png';
+import { FooterLinks } from '../utils';
+import telegram from '../assets/telegram.png';
+import medium from '../assets/medium..png';
+import discord from '../assets/discord.png';
+import X_twitter from '../assets/X_twitter.png';
 export default function Footer() {
-	return(
-		<Box width={'100%'} textAlign={'center'} bg={'#fff'} position={'relative'} zIndex={2}>
-			<Flex width={{base: '95%', xl: '1320px'}} m={'0 auto'} py={{base: '0.5rem', md: '1rem', lg: '1.4rem', xl: '1.8rem'}} justifyContent={'space-between'} alignItems={'center'} direction={{base: 'column', lg: 'row'}}>
-				<Flex gap={{base: 0, sm: '0.6rem'}} alignItems={'center'} justifyContent={'center'}>
-					<Image src={Logo} width={{base: '1.6rem', sm: '1.8rem',md: '2rem', lg: '2.2rem', xl: '2.4rem'}} height={{base: '1.6rem', sm: '1.8rem',md: '2rem', lg: '2.2rem', xl: '2.4rem'}} mr={{base: '1rem', lg:'1.2rem', xl:'1.4rem'}}/>
-					<Text whiteSpace={'nowrap'} fontSize={{base: '1.2rem', md: '1.4rem'}}>Copyright © KTX.Finance, 2024. All rights reserved</Text>
-				</Flex>
+  return (
+    <Box
+      width={'100%'}
+      textAlign={'center'}
+      bg={'#fff'}
+      position={'relative'}
+      zIndex={2}
+    >
+      <Flex
+        width={{ base: '95%', xl: '1320px' }}
+        m={'0 auto'}
+        py={{ base: '0.5rem', md: '1rem', lg: '1.4rem', xl: '1.8rem' }}
+        justifyContent={'space-between'}
+        alignItems={'center'}
+        direction={{ base: 'column', lg: 'row' }}
+      >
+        <Flex
+          gap={{ base: 0, sm: '0.6rem' }}
+          alignItems={'center'}
+          justifyContent={'center'}
+        >
+          <Image
+            src={Logo}
+            width={{
+              base: '1.6rem',
+              sm: '1.8rem',
+              md: '2rem',
+              lg: '2.2rem',
+              xl: '2.4rem',
+            }}
+            height={{
+              base: '1.6rem',
+              sm: '1.8rem',
+              md: '2rem',
+              lg: '2.2rem',
+              xl: '2.4rem',
+            }}
+            mr={{ base: '1rem', lg: '1.2rem', xl: '1.4rem' }}
+          />
+          <Text
+            whiteSpace={'nowrap'}
+            fontSize={{ base: '1.2rem', md: '1.4rem' }}
+          >
+            Copyright © KTX.Finance, 2024. All rights reserved
+          </Text>
+        </Flex>
 
-				<Flex justifyContent={'flex-end'} alignItems={'center'} gap={{base: '0', sm: '0.8rem',md: '2rem', lg: '2rem', xl: '4rem'}} direction={{base: 'column', sm: 'row'}}>
-					<Flex>
-						{FooterLinks.map(item => {
-							return <Link px={{base: '1rem', sm: '0.8rem',md: '2rem', lg: '2rem', xl: '4rem'}} py={'1rem'} href={item.url} key={item.name} _hover={{}} fontSize={{base: '1.2rem', md: '1.4rem'}}>{item.name}</Link>
-						})}
-					</Flex>
-					<Flex gap={{base: '1rem', sm: '0.8rem',md: '2rem', lg: '2rem', xl: '4rem'}} height={{base: '2rem', md: '3rem'}}>
-						<Link width={{base: '2rem', md: '3rem'}} height={{base: '2rem', md: '3rem'}}><Image src={telegram}/></Link>
-						<Link width={{base: '2rem', md: '3rem'}} height={{base: '2rem', md: '3rem'}}><Image src={medium}/></Link>
-						<Link width={{base: '2rem', md: '3rem'}} height={{base: '2rem', md: '3rem'}}><Image src={X_twitter}/></Link>
-						<Link width={{base: '2rem', md: '3rem'}} height={{base: '2rem', md: '3rem'}}><Image src={discord}/></Link>
-					</Flex>
-				</Flex>
-			</Flex>
-		</Box>
-	)
+        <Flex
+          justifyContent={'flex-end'}
+          alignItems={'center'}
+          gap={{ base: '0', sm: '0.8rem', md: '2rem', lg: '2rem', xl: '4rem' }}
+          direction={{ base: 'column', sm: 'row' }}
+        >
+          <Flex>
+            {FooterLinks.map((item) => {
+              return (
+                <Link
+                  px={{
+                    base: '1rem',
+                    sm: '0.8rem',
+                    md: '2rem',
+                    lg: '2rem',
+                    xl: '4rem',
+                  }}
+                  py={'1rem'}
+                  href={item.url}
+                  key={item.name}
+                  _hover={{}}
+                  fontSize={{ base: '1.2rem', md: '1.4rem' }}
+                >
+                  {item.name}
+                </Link>
+              );
+            })}
+          </Flex>
+          <Flex
+            gap={{
+              base: '1rem',
+              sm: '0.8rem',
+              md: '2rem',
+              lg: '2rem',
+              xl: '4rem',
+            }}
+            height={{ base: '2rem', md: '3rem' }}
+          >
+            <Link
+              width={{ base: '2rem', md: '3rem' }}
+              height={{ base: '2rem', md: '3rem' }}
+            >
+              <Image src={telegram} />
+            </Link>
+            <Link
+              width={{ base: '2rem', md: '3rem' }}
+              height={{ base: '2rem', md: '3rem' }}
+            >
+              <Image src={medium} />
+            </Link>
+            <Link
+              width={{ base: '2rem', md: '3rem' }}
+              height={{ base: '2rem', md: '3rem' }}
+            >
+              <Image src={X_twitter} />
+            </Link>
+            <Link
+              width={{ base: '2rem', md: '3rem' }}
+              height={{ base: '2rem', md: '3rem' }}
+            >
+              <Image src={discord} />
+            </Link>
+          </Flex>
+        </Flex>
+      </Flex>
+    </Box>
+  );
 }
